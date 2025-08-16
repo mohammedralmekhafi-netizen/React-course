30f0c442cb38e019ddb03e7fbf424b20268ef19d
Mekhlafi <mohammed.r.almekhafi@gmail.com>
2025-08-16 12:04:43 +0300

    more js notes

--- index.js
+++ index.js
@@ -8,3 +8,44 @@ function sayHello () {
 }
 
 sayHello();
+
+
+let square = number => number * number;
+console.log(square(5)); // 25
+square = () => 5 * 5 ;
+
+
+console.log(square()); //25 
+
+const jobs = [
+    {id : 1 , isActive : true},
+    {id : 2 , isActive : false},
+    {id : 3 , isActive : true},
+]
+
+const activeJobs = jobs.filter(function(job) {return job.isActive;});
+
+const atctiveJobs2 = jobs.filter(job => job.isActive);
+
+console.log(activeJobs); // [{id : 1 , isActive : true}, {id : 3 , isActive : true}]
+console.log(atctiveJobs2); // [{id : 1 , isActive : true
+
+
+// Arrow functions don't rebind `this`
+const person = {   
+    name : 'John',
+
+    sayHello : function() {
+        console.log(`Hello, my name is ${this.name}`);
+    }   
+}
+person.sayHello(); // Hello, my name is John
+const person2 = {   
+    name : 'John',
+
+    sayHello : () => {
+        console.log(`Hello, my name is ${this.name}`);
+    }   
+}   
+
+person2.sayHello(); // Hello, my name is undefined
